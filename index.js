df173387e624da8fddc1898fbaf8dab157dcd173
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-11-07 18:56:03 -0200

    Added babel-preset-taller-react.

---
+++ index.js
@@ -0,0 +1,9 @@
+const presetTaller = require('babel-preset-taller')
+const presetReact = require('babel-preset-react')
+
+module.exports = {
+  presets: [
+    presetTaller,
+    presetReact
+  ]
+};
